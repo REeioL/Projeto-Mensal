9ac30cefd25de67f4ae4dfdb6592709a46a8f24c
João <joaovictormelo43@gmail.com>
2024-10-31 19:17:08 -0300

    Atualização

--- index.js
+++ index.js
@@ -171,6 +171,6 @@ function removerSimbolosEAcentos(texto) {
     element.value = converterMaiusculas(element.value);
     moverInput(element, proximoId, anteriorId);
   }
-
+//
 // Inicializa o jogo ao carregar
 inicializarJogo();
